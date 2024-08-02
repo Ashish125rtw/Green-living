9fe4b3e1e23f074061cb228e89dabba2292b8021
Ashish125rtw <Workemailforme44@gmail.com>
2024-08-02 18:55:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+document
+  .getElementById("calculatorForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const electricity = parseFloat(
+      document.getElementById("electricity").value
+    );
+    const gas = parseFloat(document.getElementById("gas").value);
+    const fuel = parseFloat(document.getElementById("fuel").value);
+    const miles = parseFloat(document.getElementById("miles").value);
+
+    const electricityFootprint = electricity * 0.92; 
+    const gasFootprint = gas * 5.3;
+    const fuelFootprint = fuel * 19.6;r
+    const milesFootprint = miles * 0.79; 
+
+    const totalFootprint =
+      electricityFootprint + gasFootprint + fuelFootprint + milesFootprint;
+
+    document.getElementById(
+      "result"
+    ).innerHTML = `<h3>Your estimated monthly carbon footprint is ${totalFootprint.toFixed(
+      2
+    )} kg CO2e</h3>`;
+  });
