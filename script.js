b878c0d6ae01b3078bc61f15daced5fce70fe7de
Ashish125rtw <Workemailforme44@gmail.com>
2024-08-06 08:24:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-document
-  .getElementById("calculatorForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    const electricity = parseFloat(
-      document.getElementById("electricity").value
-    );
-    const gas = parseFloat(document.getElementById("gas").value);
-    const fuel = parseFloat(document.getElementById("fuel").value);
-    const miles = parseFloat(document.getElementById("miles").value);
+document.getElementById('calculatorForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const electricity = parseFloat(document.getElementById('electricity').value);
+  const gas = parseFloat(document.getElementById('gas').value);
+  const fuel = parseFloat(document.getElementById('fuel').value);
+  const miles = parseFloat(document.getElementById('miles').value);
 
-    const electricityFootprint = electricity * 0.92; 
-    const gasFootprint = gas * 5.3;
-    const fuelFootprint = fuel * 19.6;r
-    const milesFootprint = miles * 0.79; 
+  const electricityFootprint = electricity * 0.92; 
+  const gasFootprint = gas * 5.3; 
+  const fuelFootprint = fuel * 19.6; 
+  const milesFootprint = miles * 0.79; 
 
-    const totalFootprint =
-      electricityFootprint + gasFootprint + fuelFootprint + milesFootprint;
-
-    document.getElementById(
-      "result"
-    ).innerHTML = `<h3>Your estimated monthly carbon footprint is ${totalFootprint.toFixed(
-      2
-    )} kg CO2e</h3>`;
-  });
+  const totalFootprint = electricityFootprint + gasFootprint + fuelFootprint + milesFootprint;
+  
+  document.getElementById('result').innerHTML = `<h3>Your estimated monthly carbon footprint is ${totalFootprint.toFixed(2)} kg CO2e</h3>`;
+});
